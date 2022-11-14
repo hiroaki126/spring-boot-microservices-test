9452edf4f985087b4a1e5d06fc8d80ce162aebad
hiroaki126 <hiroaki.morimoto.126@gmail.com>
2022-11-14 21:45:46 +0800

    add backend service

---
+++ Backend/BackendService/src/main/java/com/lightfeather/BackendService/BackendServiceApplication.java
@@ -0,0 +1,23 @@
+package com.lightfeather.BackendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class BackendServiceApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServiceApplication.class, args);
+	}
+
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+}
